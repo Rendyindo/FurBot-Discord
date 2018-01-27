3807d198dea05df1d0368652dc7ff80ad8ea63c6
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-01-27 19:50:25 +0700

    use isinstance instead of type,

--- bot.py
+++ bot.py
@@ -28,10 +28,11 @@ async def on_ready():
 
 @bot.command()
 async def search(ctx, *args, description="Searches e621 with given queries"):
-    if not type(ctx) is DMChannel:
-        if not ctx.channel.is_nsfw():
-            await ctx.send("Cannot be used in non-NSFW channels!")
-            return
+    if not isinstance(ctx.channel, discord.DMChannel):
+        if not isinstance(ctx.channel, discord.GroupChannel)
+            if not ctx.channel.is_nsfw():
+                await ctx.send("Cannot be used in non-NSFW channels!")
+                return
     headers = {
         'User-Agent': 'SearchBot/1.0 (by Error- on e621)'
     }
