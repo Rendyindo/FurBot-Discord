3eaed6493412977d0687005028159ad3b80d5082
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-01-31 16:43:07 +0700

    Re:write commands

--- bot.py
+++ bot.py
@@ -13,8 +13,55 @@ try:
 except KeyError:
     pass
 
+class ResultNotFound(Exception):
+    pass
+
 description = '''Bot that uhh, actually just searches e621'''
 bot = commands.Bot(command_prefix='e621!', description=description)
+headers = {
+    'User-Agent': 'SearchBot/1.0 (by Error- on e621)'
+}
+
+def processapi(apilink):
+    print("API Link: " + apilink)
+    print("Requesting json from API")
+    r = requests.get(url=apilink, headers=headers)
+    datajson = r.json()
+    if not datajson:
+        print("Result Not Found")
+        raise ResultNotFound()
+    print("Shuffling data from json")
+    data = shuffle(datajson)
+    print("Parsing data from json")
+    fileurl = data[0]['file_url']
+    imgartists = data[0]['artist']
+    imgartist = ''.join(imgartists)
+    imgtag = data[0]['tags']
+    imgtag = imgtag.split(" ")
+    tags = [imgtag[x:x+25] for x in range(0, len(imgtag), 25)]
+    imgtags = tags[0]
+    imgrate = data[0]['rating']
+    if imgrate == "e":
+        processapi.imgrating = "Explicit"
+    if imgrate == "s":
+        processapi.imgrating = "Safe"
+    if imgrate == "q":
+        processapi.imgrating = "Mature/Questionable"
+    imgsources = data[0]['source']
+    imgsource = str(imgsources)
+    if imgartist == "None":
+        processapi.imgartist = "Unspecified"
+    else:
+        processapi.imgartist = imgartist
+    if imgsource == "None":
+        processapi.imgsource = "Unspecified"
+    else:
+        processapi.imgsource = imgsource
+    processapi.imgtags = str(' '.join(imgtags))
+    imgid = data[0]['id']
+    processapi.imgid = str(imgid)
+    processapi.file_link = str(fileurl).replace('None', '')
+
 def shuffle(arr):
     random.shuffle(arr)
     return arr
@@ -34,49 +81,17 @@ async def search(ctx, *args, description="Searches e621 with given queries"):
             if not ctx.channel.is_nsfw():
                 await ctx.send("Cannot be used in non-NSFW channels!")
                 return
-    headers = {
-        'User-Agent': 'SearchBot/1.0 (by Error- on e621)'
-    }
     args = ' '.join(args)
     args = str(args)
     print("------")
     print("Got command with args: " + args)
     apilink = 'https://e621.net/post/index.json?tags=' + args + '&limit=320'
-    print("API Link: " + apilink)
-    print("Requesting json from API")
-    r = requests.get(url=apilink, headers=headers)
-    datajson = r.json()
-    if not datajson:
-        await ctx.send("No result for that query")
+    try:
+        processapi(apilink)
+    except ResultNotFound:
+        await ctx.send("Result not found!")
         return
-    print("Shuffling data from json")
-    data = shuffle(datajson)
-    print("Parsing data from json")
-    fileurl = data[0]['file_url']
-    imgartists = data[0]['artist']
-    imgartist = ''.join(imgartists)
-    imgtag = data[0]['tags']
-    imgtag = imgtag.split(" ")
-    tags = [imgtag[x:x+25] for x in range(0, len(imgtag), 25)]
-    imgtags = tags[0]
-    imgrate = data[0]['rating']
-    if imgrate == "e":
-        imgrating = "Explicit"
-    if imgrate == "s":
-        imgrating = "Safe"
-    if imgrate == "q":
-        imgrating = "Mature/Questionable"
-    imgsources = data[0]['source']
-    imgsource = str(imgsources)
-    if imgartist == "None":
-        imgartist = "Unspecified"
-    if imgsource == "None":
-        imgsource = "Unspecified"
-    imgtags = str(' '.join(imgtags))
-    imgid = data[0]['id']
-    imgid = str(imgid)
-    file_link = str(fileurl).replace('None', '')
-    await ctx.send("""Post link: `https://e621.net/post/show/""" + imgid + """/`\r\nArtist: """ + imgartist + """\r\nSource: `""" + imgsource + """`\r\nRating: """ + imgrating + """\r\nTags: `""" + imgtags + """` ...and more\r\nImage link: """ + file_link)
+    await ctx.send("""Post link: `https://e621.net/post/show/""" + processapi.imgid + """/`\r\nArtist: """ + processapi.imgartist + """\r\nSource: `""" + processapi.imgsource + """`\r\nRating: """ + processapi.imgrating + """\r\nTags: `""" + processapi.imgtags + """` ...and more\r\nImage link: """ + processapi.file_link)
 
 bot.remove_command('help')
 @bot.command()
@@ -98,9 +113,6 @@ async def show(ctx, arg):
             if not ctx.channel.is_nsfw():
                 await ctx.send("Cannot be used in non-NSFW channels!")
                 return
-    headers = {
-        'User-Agent': 'SearchBot/1.0 (by Error- on e621)'
-    }
     print("------")
     arg = str(arg)
     print("Got command with arg: " + arg)
@@ -141,42 +153,14 @@ async def randompick(ctx, *args, description="Output random result"):
             if not ctx.channel.is_nsfw():
                 await ctx.send("Cannot be used in non-NSFW channels!")
                 return
-    headers = {
-        'User-Agent': 'SearchBot/1.0 (by Error- on e621)'
-    }
     print("------")
     print("Got command")
     apilink = 'https://e621.net/post/index.json?tags=score:>200 rating:e&limit=320'
-    print("Requesting json from API")
-    r = requests.get(url=apilink, headers=headers)
-    datajson = r.json()
-    print("Shuffling data from json")
-    data = shuffle(datajson)
-    print("Parsing data from json")
-    fileurl = data[0]['file_url']
-    imgartists = data[0]['artist']
-    imgartist = ''.join(imgartists)
-    imgtag = data[0]['tags']
-    imgtag = imgtag.split(" ")
-    tags = [imgtag[x:x+25] for x in range(0, len(imgtag), 25)]
-    imgtags = tags[0]
-    imgrate = data[0]['rating']
-    if imgrate == "e":
-        imgrating = "Explicit"
-    if imgrate == "s":
-        imgrating = "Safe"
-    if imgrate == "q":
-        imgrating = "Mature/Questionable"
-    imgsources = data[0]['source']
-    imgsource = str(imgsources)
-    if imgartist == "None":
-        imgartist = "Unspecified"
-    if imgsource == "None":
-        imgsource = "Unspecified"
-    imgtags = str(' '.join(imgtags))
-    imgid = data[0]['id']
-    imgid = str(imgid)
-    file_link = str(fileurl).replace('None', '')
-    await ctx.send("""Post link: `https://e621.net/post/show/""" + imgid + """/`\r\nArtist: """ + imgartist + """\r\nSource: `""" + imgsource + """`\r\nRating: """ + imgrating + """\r\nTags: `""" + imgtags + """` ...and more\r\nImage link: """ + file_link)
+    try:
+        processapi(apilink)
+    except ResultNotFound:
+        await ctx.send("Result not found!")
+        return
+    await ctx.send("""Post link: `https://e621.net/post/show/""" + processapi.imgid + """/`\r\nArtist: """ + processapi.imgartist + """\r\nSource: `""" + processapi.imgsource + """`\r\nRating: """ + processapi.imgrating + """\r\nTags: `""" + processapi.imgtags + """` ...and more\r\nImage link: """ + processapi.file_link)
 
 bot.run(token)
