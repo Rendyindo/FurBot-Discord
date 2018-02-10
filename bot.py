4eed37db479416fb355387fbcbd3efa043abacf8
Rendyindo <rendyarya22@gmail.com>
2018-02-10 17:00:00 +0700

    convert ID to int first

--- bot.py
+++ bot.py
@@ -304,7 +304,7 @@ async def report(ctx, *args):
     with open("reports.log", "w") as text_file:
         text_file.write(message)
     await ctx.send("Thanks for your input! I've notified my owner about it~")
-    user = bot.get_user(owner)
+    user = bot.get_user(int(owner))
     await user.send("""New report:\r
 ```""" + message + "```")
 
