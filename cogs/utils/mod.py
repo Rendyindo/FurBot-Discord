02b03635317fd29ca1e709d869f416ba433e6477
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-02-17 19:46:20 +0700

    modify mod names

--- cogs/utils/mod.py
+++ cogs/utils/mod.py
@@ -5,38 +5,36 @@ from cogs.utils.bit_enum import BitEnum
 class Mod(BitEnum):
     """The mods in osu!
     """
-    no_fail = 1
-    easy = 1 << 1
+    NF = 1
+    EZ = 1 << 1
     no_video = 1 << 2  # not a mod anymore
-    hidden = 1 << 3
-    hard_rock = 1 << 4
-    sudden_death = 1 << 5
-    double_time = 1 << 6
-    relax = 1 << 7
-    half_time = 1 << 8
-    nightcore = 1 << 9  # always used with double_time
-    flashlight = 1 << 10
-    autoplay = 1 << 11
-    spun_out = 1 << 12
-    relax2 = 1 << 13  # same as autopilot
-    auto_pilot = 1 << 13  # same as relax2
-    perfect = 1 << 14
-    key4 = 1 << 15
-    key5 = 1 << 16
-    key6 = 1 << 17
-    key7 = 1 << 18
-    key8 = 1 << 19
-    fade_in = 1 << 20
-    random = 1 << 21
-    last_mod = 1 << 22  # same as cinema
-    cinema = 1 << 22  # same as last_mod
-    target_practice = 1 << 23
-    key9 = 1 << 24
-    coop = 1 << 25
-    key1 = 1 << 26
-    key3 = 1 << 27
-    key2 = 1 << 28
-    scoreV2 = 1 << 29
+    HD = 1 << 3
+    HR = 1 << 4
+    SD = 1 << 5
+    DT = 1 << 6
+    RX = 1 << 7
+    HT = 1 << 8
+    NC = 1 << 9  # always used with double_time
+    FL = 1 << 10
+    Auto = 1 << 11
+    SO = 1 << 12
+    AP = 1 << 13  # same as autopilot
+    PF = 1 << 14
+    4K = 1 << 15
+    5K = 1 << 16
+    6K = 1 << 17
+    7K = 1 << 18
+    8K = 1 << 19
+    FadeIn = 1 << 20
+    Random = 1 << 21
+    Cinema = 1 << 22  # same as last_mod
+    TP = 1 << 23
+    9K = 1 << 24
+    Coop = 1 << 25
+    1K = 1 << 26
+    3K = 1 << 27
+    2K = 1 << 28
+    ScoreV2 = 1 << 29
 
     @classmethod
     def parse(cls, cs):
