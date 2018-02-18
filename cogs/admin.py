4b8a130caaef75d2a5fcb53cca7b79e9476dcd76
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-02-18 15:37:27 +0700

    Role: set and delete set is basically a copy of role group lol

--- cogs/admin.py
+++ cogs/admin.py
@@ -15,5 +15,25 @@ class Admin():
             await user.add_roles(role)
             await ctx.send("Set role {} for {}!".format(args, user.mention)
 
+    @role.command()
+    async def set(self, ctx, userid, *args):
+        args = ' '.join(args)
+        args = str(args)
+        mentions = ctx.message.mentions
+        for user in mentions:
+            role = discord.utils.get(ctx.guild.roles, name=args)
+            await user.add_roles(role)
+            await ctx.send("Set role {} for {}!".format(args, user.mention)
+
+    @role.command()
+    async def remove(self, ctx, userid, *args):
+        args = ' '.join(args)
+        args = str(args)
+        mentions = ctx.message.mentions
+        for user in mentions:
+            role = discord.utils.get(ctx.guild.roles, name=args)
+            await user.remove_roles(role)
+            await ctx.send("Remove role {} for {}!".format(args, user.mention)
+
 def setup(bot):
     bot.add_cog(Admin(bot))
