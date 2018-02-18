524ea1965290e35f864d0133637589e44a20a395
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-02-18 09:30:25 +0700

    smh my head

--- cogs/furry.py
+++ cogs/furry.py
@@ -117,8 +117,8 @@ class Furry():
             return
         await ctx.send("""Post link: `https://""" + netloc + """.net/post/show/""" + processapi.imgid + """/`\r\nArtist: `""" + processapi.imgartist + """`\r\nSource: `""" + processapi.imgsource + """`\r\nRating: """ + processapi.imgrating + """\r\nTags: `""" + processapi.imgtags + """` ...and more\r\nImage link: """ + processapi.file_link)
         
-        @commands.command(pass_context=True)
-        async def sofurry(self, ctx, *args):
+    @commands.command(pass_context=True)
+    async def sofurry(self, ctx, *args):
         """Searches SoFurry with given queries.
 
         Arguments:
